37c4064015c8b9e7f5a75fdb0ff01eaa44458b64
sarthakkanungo <sarthak.kanungo001@gmail.com>
2023-06-08 23:44:00 +0530

    context Api to stop prop chain

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useReducer } from "react";
+import React, {useEffect, useState, useReducer } from "react";
 
 import Card from "../UI/Card/Card";
 import classes from "./Login.module.css";
